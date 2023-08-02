e3275f69e09a3eccde621e5a209b10498b46423f
Rakesh-kumarjha <mr.rakeshjha.official@gmail.com>
2023-08-02 15:55:08 +0530

    new commit

--- src/components/Sats.jsx
+++ src/components/Sats.jsx
@@ -11,7 +11,7 @@ export default function Stats() {
                 <h2 className="lg:text-6xl md:text-5xl text-4xl font-black leading-10 text-white">By the numbers</h2>
               </div>
               <div className="mt-6 mx-2 md:mx-0 text-center">
-                <p className="lg:text-lg md:text-base leading-6 text-sm  text-white">5 years, consistent quality and you get results. No matter what</p>
+                <p className="lg:text-lg md:text-base leading-6 text-sm  text-white">Consistent quality and you get results. No matter what</p>
               </div>
             </div>
           </div>
